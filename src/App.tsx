6c9dafea624a41a7f9537dc9bf2b2b07382c6f5a
dariia-zhukovska <dariia.zhukovska@gmail.com>
2023-04-13 16:12:02 +0300

    deleted some imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import PageLayout from './components/PageLayout/PageLayout';
-import logo from './logo.svg';
-import './App.css';
-
+import PageLayout from "./components/PageLayout/PageLayout";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <PageLayout/>
+      <PageLayout />
     </div>
   );
 }
